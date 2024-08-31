e52dd413dd91c1462c07f1aacb67b29f1a68b29f
Arash KORDI <arash.kordi@vestiairecollective.com>
2024-08-31 18:16:28 +0200

    add page to trending

--- trending.go
+++ trending.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 )
 
-
 // GetTrending get the daily or weekly trending items.
 // https://developers.themoviedb.org/3/trending/get-trending
-func (tmdb *TMDb) GetTrending(media_type,time_window string) (*MoviePagedResults, error) {
+func (tmdb *TMDb) GetTrending(media_type, time_window string, page int) (*MoviePagedResults, error) {
 	var nowPlaying MoviePagedResults
-	uri := fmt.Sprintf("%s/trending/%v/%v?api_key=%s", baseURL, media_type, time_window, tmdb.apiKey)
+	uri := fmt.Sprintf("%s/trending/%v/%v?page=%d&api_key=%s", baseURL, media_type, time_window, page, tmdb.apiKey)
 	result, err := getTmdb(uri, &nowPlaying)
 	return result.(*MoviePagedResults), err
 }
